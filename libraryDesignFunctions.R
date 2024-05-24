9d9163aa7b0c6b45b1aef905f1a869f0b3c8a1b0
sjpl <20521168+sjpl@users.noreply.github.com>
2024-05-24 16:00:18 -0400

    Create findSnpRegions()
    Functionalizes the creation of SNP regions and combining nearby SNPs into the same region. Have to run this function several times to get different region widths (i.e. 200, 300, 400bp).

--- libraryDesignFunctions.R
+++ libraryDesignFunctions.R
@@ -1,3 +1,60 @@
+# Author: Shannon Laub
+
+# Description: Contains functions for designScript.Rmd
+# pickTopGDO()
+# put description here...
+
+# Function wishlist
+# Check for overlaps + output dataframe w/ indices
+
+# Guide preprocessing + add guide coords
+
+# Generate Regions
+
+findSnpRegions <- function(regionLength, snpList){
+  # set half length parameter
+  halfLength = regionLength/2-1
+  
+  # Create SNP "regions". +/- 1/2 the total region width.
+  snpRegion <- snpList %>%
+    mutate(start = pos-halfLength,
+           end = pos+halfLength)
+  
+  # Use GenomicRanges to find region overlaps. Create GRanges objects to use it.
+  gRegion <- GRanges(seqnames = snpRegion$chr,
+                     ranges = IRanges(start = snpRegion$start, end=snpRegion$end))
+  
+  gSnps <- GRanges(seqnames = snpRegion$chr,
+                   ranges = IRanges(start=snpRegion$pos, end=snpRegion$pos),
+                   SNP = snpRegion$snp)
+  
+  # Merge overlapping SNP regions
+  gRegion <- reduce(gRegion)
+  
+  
+  # Map SNPs back to merged regions.
+  gOverlap <- findOverlaps(gRegion, gSnps)
+  
+  # Convert regions from GRanges object to dataframe
+  regions <- as.data.frame(gRegion) 
+  regions$SNP <- NA
+  names(regions)[names(regions)=="seqnames"] <- "chr"
+  
+  # Convert snps from GRanges object to dataframe
+  snps <- as.data.frame(gSnps)
+  
+  # Map SNPs to their regions.
+  for(i in 1:nrow(regions)){
+    # i = 5
+    reg_idx <- which(gOverlap@from==i) #get index for region
+    snp_idx <- gOverlap@to[reg_idx] # map to index for SNP
+    
+    regions[i, "SNP"] <- paste0(snps$SNP[snp_idx],collapse="|")
+  }
+  
+  
+  return(regions)
+}
 
 pickTopGDO <- function(numTopGDO, guidePool, regions, overlapGDO) {
   topGDO <- guidePool[0,]
@@ -7,6 +64,7 @@ pickTopGDO <- function(numTopGDO, guidePool, regions, overlapGDO) {
     for (j in 1:nrow(regions)) {
       # j = 1
       if (length(subset(guidePool, SNP==regions$SNP[j]))>0){
+        
         top <- guidePool %>%
           subset(SNP==regions$SNP[j]) %>%
           arrange(Pick.Order) %>%
